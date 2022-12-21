5efcfc8c8509b2f07f0a3f1689035b27af8d4b9d
matheus costa <matheuscosta.tech@gmail.com>
2022-12-21 11:38:22 -0300

    Finished cancel notification

--- src/application/usecases/cancel-notification.spec.ts
+++ src/application/usecases/cancel-notification.spec.ts
@@ -2,6 +2,7 @@ import { Notification } from '@application/entities/notification';
 import { Content } from '@application/entities/notification-content';
 import { InMemoryNotificationRepository } from '../../test/in-memory-notification-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
     it('should be able to send a notification', async () => {
@@ -23,6 +24,15 @@ describe('Cancel notification', () => {
             expect.any(Date),
         );
     });
-});
 
-// video stopped at 42:27
+    it('should not be able to cancel a non existing notification', () => {
+        const repository = new InMemoryNotificationRepository();
+        const cancelNotification = new CancelNotification(repository);
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-id',
+            });
+        }).rejects.toThrow(NotificationNotFound);
+    });
+});
