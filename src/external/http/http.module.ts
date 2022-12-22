5b3c894fa7eab25de249c2a82b52867310ad5894
matheus costa <matheuscosta.tech@gmail.com>
2022-12-21 22:41:59 -0300

    Passing the usecases to the module

--- src/external/http/http.module.ts
+++ src/external/http/http.module.ts
@@ -1,13 +1,23 @@
-import { Module } from "@nestjs/common";
-import { SendNotification } from "src/application/usecases/send-notification";
-import { DatabaseModule } from "../database/database.module";
-import { NotificationsController } from "./controllers/notifications.controller";
+import { CancelNotification } from '@application/usecases/cancel-notification';
+import { CountRecipientNotifications } from '@application/usecases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/usecases/get-recipient-notifications';
+import { ReadNotification } from '@application/usecases/read-notification';
+import { UnreadNotification } from '@application/usecases/unread-notifications';
+import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/usecases/send-notification';
+import { DatabaseModule } from '../database/database.module';
+import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
     providers: [
         SendNotification,
-    ]
+        CancelNotification,
+        ReadNotification,
+        UnreadNotification,
+        CountRecipientNotifications,
+        GetRecipientNotifications,
+    ],
 })
-export class HttpModule {};
+export class HttpModule {}
