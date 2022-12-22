2de99b44a048dee2452c498aa36d1ec479b9e8cb
matheus costa <matheuscosta.tech@gmail.com>
2022-12-21 22:39:05 -0300

    Building the url routes

--- src/external/http/controllers/notifications.controller.ts
+++ src/external/http/controllers/notifications.controller.ts
@@ -1,23 +1,76 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { CancelNotification } from '@application/usecases/cancel-notification';
+import { CountRecipientNotifications } from '@application/usecases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/usecases/get-recipient-notifications';
+import { ReadNotification } from '@application/usecases/read-notification';
+import { UnreadNotification } from '@application/usecases/unread-notifications';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/usecases/send-notification';
 import { CreateNotificationBody } from '../../dtos/create-notification-body';
 import { HttpNotificationMapper } from '../mappers/notification-mapper';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+    constructor(
+        private sendNotification: SendNotification,
+        private cancelNotification: CancelNotification,
+        private readNotification: ReadNotification,
+        private unreadNotification: UnreadNotification,
+        private countRecipientNotifications: CountRecipientNotifications,
+        private getRecipientNotifications: GetRecipientNotifications,
+    ) {}
 
-  @Post()
-  async create(@Body() body: CreateNotificationBody) {
-    const { recipientId, content, category } = body;
+    @Patch(':id/cancel')
+    async cancel(@Param('id') id: string) {
+        await this.cancelNotification.execute({
+            notificationId: id,
+        });
+    }
 
-    const { notification } = await this.sendNotification.execute({
-        recipientId,
-        content,
-        category,
-    });
-    return {
-        notification: HttpNotificationMapper.toHttp(notification),
-    };
-  }
+    @Get('count/from/:recipientId')
+    async countFromRecipient(@Param('recipientId') recipientId: string) {
+        const { count } = await this.countRecipientNotifications.execute({
+            recipientId,
+        });
+
+        return { count };
+    }
+
+    @Get('from/:recipientId')
+    async getFromRecipient(@Param('recipientId') recipientId: string) {
+        const { notifications } = await this.getRecipientNotifications.execute({
+            recipientId,
+        });
+
+        return {
+            notifications: notifications.map(HttpNotificationMapper.toHttp),
+        };
+    }
+
+    @Patch(':id/read')
+    async read(@Param('id') id: string) {
+        await this.readNotification.execute({
+            notificationId: id,
+        });
+    }
+
+    @Patch(':id/unread')
+    async unread(@Param('id') id: string) {
+        await this.unreadNotification.execute({
+            notificationId: id,
+        });
+    }
+
+    @Post()
+    async create(@Body() body: CreateNotificationBody) {
+        const { recipientId, content, category } = body;
+
+        const { notification } = await this.sendNotification.execute({
+            recipientId,
+            content,
+            category,
+        });
+        return {
+            notification: HttpNotificationMapper.toHttp(notification),
+        };
+    }
 }
